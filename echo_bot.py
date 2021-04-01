6c646d55c28ba8d0d8b70872eaf92efefc0e0d57
neEZH <47315839+neEZH@users.noreply.github.com>
2021-04-01 09:54:08 +0300

    Initial
    Initial version of bot.
    no normalizin
    no refactor
    no nice exeptions
    just hardcode
    //todo

---
+++ echo_bot.py
@@ -0,0 +1,55 @@
+import telebot
+import requests
+import json
+import os
+import sys
+
+# https://www.artstation.com/users/[artistName]/projects.json
+
+bot = telebot.TeleBot(os.environ['botToken'])
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f"Hello there")
+
+
+@bot.message_handler(commands=['get'])
+def getMessageInfo(message):
+    print("Message get")
+    chatID = message.chat.id
+
+    print("Chat ID: " + str(message.chat.id))
+
+    print("message ID: " + str(message.message_id))
+
+    print("from: " + str(message.from_user.username))
+
+    print("text: " + str(message.text.split(" ")))
+    try:
+        artistName = message.text.split(" ")[1]
+        url = "https://www.artstation.com/users/" + artistName + "/projects.json"
+
+        rsp = json.loads(requests.get(url).text)
+
+        lastPubl = ""
+        lastId = 0
+        artId = 0
+        for artWork in rsp['data']:
+            if lastPubl < artWork['published_at']:
+                lastPubl = artWork['published_at']
+                lastId = artId
+            artId += 1
+        artUrl = rsp['data'][lastId]['permalink']
+        thumbUrl = rsp['data'][lastId]['cover']['thumb_url']
+
+        bot.send_photo(chatID, thumbUrl, artUrl)
+    #except IndexError as exp:
+    #    bot.send_message(chatID, 'Добавь Ник художника: "/get ArtistName"')
+    except Exception as exp:
+        bot.send_message(chatID, "Error: " + str(exp))
+
+
+print("Bot starting!!")
+sys.stdout.write("Log: Start")
+bot.polling(none_stop=True)
