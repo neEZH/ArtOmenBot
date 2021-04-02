2e2fef537913fa0129b2fbc87ede126977fc4239
neEZH <neezhassa@gmail.com>
2021-04-03 02:56:30 +0300

    artstation.py class for AS work with

--- artstation.py
+++ artstation.py
@@ -5,22 +5,23 @@ class AS:
     def __init__(self, name):
         self.name = name
 
+    @property
     def ifArtist(self):
         return bool(requests.get("https://www.artstation.com/users/" + str(self.name) + "/projects.json").text)
 
+    @property
     def ifProjs(self):
         return bool(self.getProjs()['total_count'])
 
+    @property
     def getProjs(self):
         return json.loads(requests.get("https://www.artstation.com/users/" + str(self.name) + "/projects.json").text)["data"]
 
+    @property
     def lastArt(self):
-        #
-        # maxDate = ""
-        # maxIx = 0
-        return next(project for project in self.getProjs() if project["published_at"] == max([project["published_at"] for project in self.getProjs()]))
-        # for prDate in (self.getProjs()["data"]["published_at"] for self.getProjs() )
-        # for proj in self.getProjs()["data"]:
-        #     if maxDate < proj["published_at"]:
-        #         maxDate = proj["published_at"]
+        #return next(project for project in self.getProjs() if project["published_at"] == max([project["published_at"] for project in self.getProjs()]))
+        maxPublDate = max([project["published_at"] for project in self.getProjs])
+        projList = self.getProjs
+        return next(project for project in projList if project["published_at"] == maxPublDate)
+
 
